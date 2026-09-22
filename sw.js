b2b9ed0667bc5a9f9d5ac5cc15b649c0c58a6ef7
ataabanoz <ckkd76h8dm@privaterelay.appleid.com>
2026-09-23 00:42:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='trabzonhava-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='trabzonhava-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
