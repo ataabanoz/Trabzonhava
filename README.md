9d8b48438aec0ee0bd4f8ce8271e1050660186e6
ataabanoz <ckkd76h8dm@privaterelay.appleid.com>
2026-09-25 02:28:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# TrabzonHava v6.9
-Doğa & Outdoor listesinden kullanıcının belirttiği 16 nokta kaldırıldı. Diğer bölümler korunmuştur.
+# TrabzonHava v7.0
+- TULAŞ / Trabzon Büyükşehir Belediyesi otobüs seferleri eklendi.
+- Trabzon nöbetçi eczaneler bölümü eklendi.
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='trabzonhava-v69';
+const CACHE='trabzonhava-v70';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await clients.claim()})()));
